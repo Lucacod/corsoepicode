e5fac068888b2ba4972d61fc5e76da30abd1ed63
Lucacod <132904180+Lucacod@users.noreply.github.com>
2023-05-29 01:36:06 +0300

    Add files via upload

---
+++ M2:D2/main.js
@@ -0,0 +1,45 @@
+/*
+STRING: è una sequenza di caratteri racchiusa tra singole o doppie apici "" o ''
+BOOLEAN: è un valore che determina se è vero o falso.
+UNDEFINED: è quando non viene assegnato nessun valore alla variabile!
+NULL: quando un valore è vuoto , la variabile non contiene niente.
+*/
+
+
+let primoNumero = 12
+let secondoNumero = 20
+
+let addizione = primoNumero + secondoNumero;
+
+console.log ('RISULTATO ADDIZIONE:  ', addizione)
+
+
+
+
+let x = 12
+let name = 'Luca'
+
+
+let sottrazione = 4 - x;
+
+console.log ('risultato sottrazione  ',sottrazione)
+
+
+
+let profile = {
+    name:"Luca",
+    surname: "Ragosta",
+    Hobby: "musica"    
+}
+
+console.log (profile)
+
+
+
+let name1 = "john"
+let name2 = "John"
+
+console.log (name1 , name2)
+
+
+
